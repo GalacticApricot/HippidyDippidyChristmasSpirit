d8fbaadc5a24720643ed9fee827e809678645081
Galactic Apricot <87578551+GalacticApricot@users.noreply.github.com>
2024-12-23 18:54:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -574,6 +574,7 @@ function continueReveal() {
     , 1e3)
 }
 function finalReveal() {
+    document.getElementById("buttons").style.visibility = 'visible';
     ripple();
     let e = document.getElementById("name");
     e.style.opacity = 1,
